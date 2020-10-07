a3bd4a7e96966e76cdfaab4aeb5b07187d311196
OnderbekeNiels <55786916+OnderbekeNiels@users.noreply.github.com>
2020-10-07 10:44:29 +0200

    password afwerking

--- script/app.js
+++ script/app.js
@@ -1,13 +1,18 @@
-function handleFloatingLabel() {
-   
-}
+function handleFloatingLabel() {}
 
 function handlePasswordSwitcher() {
-    
+  const pwd = document.querySelector(".js-password");
+  const checkbox = document.querySelector(".js-checkbox");
+  const passwordOptions = ["password", "text"];
+
+  checkbox.addEventListener("change", function () {
+    pwd.type = passwordOptions[+this.checked];
+    console.log("jow");
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Script loaded!");
+  handleFloatingLabel();
+  handlePasswordSwitcher();
 });
